f55e1654a13278281dcaf2511ed0cda61d5d0cd4
Elad Bar <eladba@payoneer.com>
2020-07-27 08:43:17 +0300

    Reordered and removed unused functions

--- networkViewer.js
+++ networkViewer.js
@@ -1,4 +1,5 @@
 
+//Variables
 const HOPS = {
     0: {
         background: "#577590",
@@ -90,7 +91,7 @@ const CAPABILITY_INFO = {
         image: "failed.png",
         title: "Failed"
     }
-}
+};
 
 
 const options = {
@@ -124,6 +125,8 @@ const options = {
     }
 };
 
+
+// Build graph
 const networkItems = [];
 let networkView = null;
 
@@ -133,7 +136,7 @@ const checkCapability = (capabilities, attributes, key) => {
     if(capability !== undefined&& eval(capability) === true) {
         capabilities.push(key);
     }
-}
+};
 
 const getCapabilities = (selectedItem) => {
     const attributes = selectedItem.data.attributes;
@@ -156,195 +159,6 @@ const getCapabilities = (selectedItem) => {
     });
 
     return capabilities;
-};
-
-const setCapabilityIcons = (selectedItem) => {
-    const capabilities = getCapabilities(selectedItem);
-
-    const capabilityImageKeys = Object.keys(CAPABILITY_INFO);
-
-    const availableCapabilities = capabilityImageKeys.filter(ck => capabilities !== undefined && capabilities.indexOf(ck) > -1);
-
-    const divIcons = document.createElement("div");
-    divIcons.className = "node-details-content-item";
-
-    availableCapabilities.forEach(ck => {
-        const img = document.createElement("img");
-        const capabilityInfo = CAPABILITY_INFO[ck];
-
-        img.src = `images/${capabilityInfo.image}`;
-        img.className = "node-capability-icon";
-        img.title = capabilityInfo.title;
-
-        divIcons.appendChild(img);
-    });
-
-    const element = document.getElementById('node-details-content');
-    element.appendChild(divIcons);
-}
-
-const setNeighbors = (selectedItem) => {
-    const neighbors = selectedItem.neighbors;
-    const divNeighbors = document.createElement("div");
-    const hasNeighbors = neighbors !== undefined;
-
-    const orderedNeighbors = {};
-
-    if (hasNeighbors) {
-        neighbors.map(n => networkItems.filter(i => i.id === n)[0])
-            .forEach(i => {
-                let hopNeighbors = orderedNeighbors[i.hop];
-
-                if(hopNeighbors === undefined) {
-                    hopNeighbors = [];
-                    orderedNeighbors[i.hop] = hopNeighbors;
-                }
-
-                hopNeighbors.push(i);
-            });
-
-        hopKeys = Object.keys(HOPS).sort();
-
-        hopKeys.forEach(hk => {
-            items = orderedNeighbors[hk];
-
-            if(items !== undefined) {
-                items.forEach(n => {
-                    const divNeighbor = document.createElement("div");
-                    divNeighbor.className = "node-neighbor-item";
-                    divNeighbor.style.color = n.color;
-                    divNeighbor.innerText = n.label;
-                    divNeighbor["nodeId"] = n.id;
-
-                    divNeighbor.onclick = neighborClicked
-
-                    divNeighbors.appendChild(divNeighbor);
-                });
-            }
-        });
-    }
-
-    const element = document.getElementById('node-neighbors-content');
-    while (element.firstChild) {
-        element.removeChild(element.lastChild);
-    }
-
-    element.appendChild(divNeighbors);
-
-    if(!hasNeighbors) {
-        element.style.display = "none";
-    }
-};
-
-const setDetailsItem = (container, title, content, breakAfter) => {
-    const div = document.createElement("div");
-    div.className = "node-details-content-item";
-
-    const titleDiv = document.createElement("div");
-    titleDiv.innerHTML = title + ":";
-    titleDiv.className = "node-details-content-item-title" + (breakAfter ? "" : " node-details-content-item-no-break");
-    div.appendChild(titleDiv);
-
-    const contentDiv = document.createElement("div");
-    contentDiv.innerHTML = content;
-
-    if (!breakAfter){
-        contentDiv.className = "node-details-content-item-no-break";
-    }
-
-    div.appendChild(contentDiv);
-
-    container.appendChild(div);
-}
-
-const setDetails = (selectedItem) => {
-    const nodeName = document.getElementById('node-details-title');
-    nodeName.innerText = selectedItem.label;
-
-    const attr_keys = Object.keys(ATTRIBUTES_MAPPING);
-    const attributes = selectedItem.data.attributes;
-    const availableAttributes = {};
-
-    attr_keys.forEach(ak => {
-        availableAttributes[ak] = attributes[ak];
-    });
-
-    availableAttributes["entity_id"] = selectedItem.data.entity_id;
-    availableAttributes["state"] = selectedItem.data.state;
-
-    const nodeDetailsContent = document.getElementById('node-details-content');
-    while (nodeDetailsContent.firstChild) {
-        nodeDetailsContent.removeChild(nodeDetailsContent.lastChild);
-    }
-
-    const div = document.createElement("div");
-    div.className = "";
-
-    attr_keys.forEach(k => {
-        const attrDetails = ATTRIBUTES_MAPPING[k];
-        const attrValue = availableAttributes[k];
-
-        if (attrValue !== undefined && attrValue !== "Unknown") {
-            setDetailsItem(div, attrDetails.title, attrValue, attrDetails.breakAfter);
-        }
-    });
-
-    nodeDetailsContent.appendChild(div);
-};
-
-const loadHopsLegend = () => {
-    const container = document.getElementById("hops-legend");
-
-    const hopLegendTitle = document.createElement("div");
-    hopLegendTitle.className = "sidebar-section-title";
-    container.appendChild(hopLegendTitle);
-
-    const hops = Object.keys(HOPS);
-
-    hops.forEach(h => {
-        const currentHop = HOPS[h];
-        const text = currentHop.title === undefined ? `Hop ${h}` : currentHop.title;
-        const currentHopClassSuffix = currentHop.classSuffix === undefined ? `hop-${h}` : currentHop.classSuffix;
-
-        const hopContent = document.createElement("div");
-        hopContent.className = "sidebar-section-content";
-
-        const hopContentBox = document.createElement("div");
-        hopContentBox.className = `legend-box legend-box-${currentHopClassSuffix}`;
-        hopContent.appendChild(hopContentBox);
-
-        const hopContentText = document.createElement("div");
-        hopContentText.className = "legend-title";
-        hopContentText.innerText = text;
-        hopContent.appendChild(hopContentText);
-
-        container.appendChild(hopContent);
-    });
-};
-
-const neighborClicked = (e) => {
-    const nodeId = e.target.nodeId;
-
-    networkView.selectNodes([nodeId]);
-
-    const selectedItem = getItem(nodeId);
-
-    selectedItemChanged(selectedItem);
-};
-
-const selectedItemChanged = (selectedItem) => {
-    const sidebar = document.getElementById("sidebar");
-
-    const isSelected = selectedItem !== null;
-    sidebar.style.display = isSelected ? "block" : "none";
-
-    if (!isSelected) {
-        return;
-    }
-
-    setDetails(selectedItem);
-    setCapabilityIcons(selectedItem);
-    setNeighbors(selectedItem);
 };
 
 const getFirstHub = () => {
@@ -463,7 +277,7 @@ const getEdges = () => {
 
 const fixItemData = () => {
     networkItems.forEach(i => {
-        const currentHop = HOPS[i.hop];
+        let currentHop = HOPS[i.hop];
 
         if(currentHop === undefined) {
             currentHop = HOPS["-1"];
@@ -471,6 +285,37 @@ const fixItemData = () => {
 
         i.color = currentHop.background;
         i.font.color = currentHop.fontColor;
+    });
+};
+
+// UI
+const loadHopsLegend = () => {
+    const container = document.getElementById("hops-legend");
+
+    const hopLegendTitle = document.createElement("div");
+    hopLegendTitle.className = "sidebar-section-title";
+    container.appendChild(hopLegendTitle);
+
+    const hops = Object.keys(HOPS);
+
+    hops.forEach(h => {
+        const currentHop = HOPS[h];
+        const text = currentHop.title === undefined ? `Hop ${h}` : currentHop.title;
+        const currentHopClassSuffix = currentHop.classSuffix === undefined ? `hop-${h}` : currentHop.classSuffix;
+
+        const hopContent = document.createElement("div");
+        hopContent.className = "sidebar-section-content";
+
+        const hopContentBox = document.createElement("div");
+        hopContentBox.className = `legend-box legend-box-${currentHopClassSuffix}`;
+        hopContent.appendChild(hopContentBox);
+
+        const hopContentText = document.createElement("div");
+        hopContentText.className = "legend-title";
+        hopContentText.innerText = text;
+        hopContent.appendChild(hopContentText);
+
+        container.appendChild(hopContent);
     });
 };
 
@@ -486,7 +331,7 @@ const loadNetworkView = (networkEdges) => {
         edges: edges
     };
 
-    const container = document.getElementById("network")
+    const container = document.getElementById("network");
 
     networkView = new vis.Network(container, data, options);
 
@@ -496,6 +341,156 @@ const loadNetworkView = (networkEdges) => {
     onSelect();
 };
 
+const setNeighbors = (selectedItem) => {
+    const neighbors = selectedItem.neighbors;
+    const divNeighbors = document.createElement("div");
+    const hasNeighbors = neighbors !== undefined;
+
+    const orderedNeighbors = {};
+
+    if (hasNeighbors) {
+        neighbors.map(n => networkItems.filter(i => i.id === n)[0])
+            .forEach(i => {
+                let hopNeighbors = orderedNeighbors[i.hop];
+
+                if(hopNeighbors === undefined) {
+                    hopNeighbors = [];
+                    orderedNeighbors[i.hop] = hopNeighbors;
+                }
+
+                hopNeighbors.push(i);
+            });
+
+        const hopKeys = Object.keys(HOPS).sort();
+
+        hopKeys.forEach(hk => {
+            const items = orderedNeighbors[hk];
+
+            if(items !== undefined) {
+                items.forEach(n => {
+                    const divNeighbor = document.createElement("div");
+                    divNeighbor.className = "node-neighbor-item";
+                    divNeighbor.style.color = n.color;
+                    divNeighbor.innerText = n.label;
+                    divNeighbor["nodeId"] = n.id;
+
+                    divNeighbor.onclick = neighborClicked;
+
+                    divNeighbors.appendChild(divNeighbor);
+                });
+            }
+        });
+    }
+
+    const element = document.getElementById('node-neighbors-content');
+    while (element.firstChild) {
+        element.removeChild(element.lastChild);
+    }
+
+    element.appendChild(divNeighbors);
+
+    if(!hasNeighbors) {
+        element.style.display = "none";
+    }
+};
+
+const setDetailsItem = (container, title, content, breakAfter) => {
+    const div = document.createElement("div");
+    div.className = "node-details-content-item";
+
+    const titleDiv = document.createElement("div");
+    titleDiv.innerHTML = title + ":";
+    titleDiv.className = "node-details-content-item-title" + (breakAfter ? "" : " node-details-content-item-no-break");
+    div.appendChild(titleDiv);
+
+    const contentDiv = document.createElement("div");
+    contentDiv.innerHTML = content;
+
+    if (!breakAfter){
+        contentDiv.className = "node-details-content-item-no-break";
+    }
+
+    div.appendChild(contentDiv);
+
+    container.appendChild(div);
+};
+
+const setDetails = (selectedItem) => {
+    const nodeName = document.getElementById('node-details-title');
+    nodeName.innerText = selectedItem.label;
+
+    const attr_keys = Object.keys(ATTRIBUTES_MAPPING);
+    const attributes = selectedItem.data.attributes;
+    const availableAttributes = {};
+
+    attr_keys.forEach(ak => {
+        availableAttributes[ak] = attributes[ak];
+    });
+
+    availableAttributes["entity_id"] = selectedItem.data.entity_id;
+    availableAttributes["state"] = selectedItem.data.state;
+
+    const nodeDetailsContent = document.getElementById('node-details-content');
+    while (nodeDetailsContent.firstChild) {
+        nodeDetailsContent.removeChild(nodeDetailsContent.lastChild);
+    }
+
+    const div = document.createElement("div");
+    div.className = "";
+
+    attr_keys.forEach(k => {
+        const attrDetails = ATTRIBUTES_MAPPING[k];
+        const attrValue = availableAttributes[k];
+
+        if (attrValue !== undefined && attrValue !== "Unknown") {
+            setDetailsItem(div, attrDetails.title, attrValue, attrDetails.breakAfter);
+        }
+    });
+
+    nodeDetailsContent.appendChild(div);
+};
+
+const setCapabilityIcons = (selectedItem) => {
+    const capabilities = getCapabilities(selectedItem);
+
+    const capabilityImageKeys = Object.keys(CAPABILITY_INFO);
+
+    const availableCapabilities = capabilityImageKeys.filter(ck => capabilities !== undefined && capabilities.indexOf(ck) > -1);
+
+    const divIcons = document.createElement("div");
+    divIcons.className = "node-details-content-item";
+
+    availableCapabilities.forEach(ck => {
+        const img = document.createElement("img");
+        const capabilityInfo = CAPABILITY_INFO[ck];
+
+        img.src = `images/${capabilityInfo.image}`;
+        img.className = "node-capability-icon";
+        img.title = capabilityInfo.title;
+
+        divIcons.appendChild(img);
+    });
+
+    const element = document.getElementById('node-details-content');
+    element.appendChild(divIcons);
+};
+
+const selectedItemChanged = (selectedItem) => {
+    const sidebar = document.getElementById("sidebar");
+
+    const isSelected = selectedItem !== null;
+    sidebar.style.display = isSelected ? "block" : "none";
+
+    if (!isSelected) {
+        return;
+    }
+
+    setDetails(selectedItem);
+    setCapabilityIcons(selectedItem);
+    setNeighbors(selectedItem);
+};
+
+// UI Events
 const onSelect = (eventData, callback) => {
     const selectedNodes = eventData === undefined ? [] : eventData.nodes;
     let selectedItem = networkItems[0];
@@ -511,14 +506,23 @@ const onSelect = (eventData, callback) => {
     selectedItemChanged(selectedItem);
 };
 
-const initialize = (entities) => {
-    loadHopsLegend();
-    setInitialHop(entities);
+const neighborClicked = (e) => {
+    const nodeId = e.target.nodeId;
 
+    networkView.selectNodes([nodeId]);
+
+    const selectedItem = getItem(nodeId);
+
+    selectedItemChanged(selectedItem);
+};
+
+// Initialize
+const initialize = (entities) => {
     loadNetworkItems(entities);
     const edges = getEdges();
 
     fixItemData();
 
+    loadHopsLegend();
     loadNetworkView(edges);
 };
